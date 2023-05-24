7698919b6d706a12f243024aed75f97ac0738655
Introvertedtechie <zillionaire234@gmail.com>
2023-05-24 08:16:55 +0100

    project

--- backend/app.js
+++ backend/app.js
@@ -5,17 +5,25 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 var cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 
 
 
-//database connection
 // Database connection
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true})
     .then(() => console.log("DB connected"))
     .catch((err) => console.log(err));
+
+
+//MIDDLEWARE
+app.use(morgan('dev'));                        app.use(bodyParser.json({ limit: "5mb" }));    app.use(bodyParser.urlencoded({                    limit: "5mb",
+    extended: true
+}));                                           app.use(cookieParser());
+app.use(cors());                                                                              // error middleware                            app.use(errorHandler);
+
 
 
 //port
